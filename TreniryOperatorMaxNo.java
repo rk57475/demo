d392a3ab9d14f6f7d8d256c2038f56abaf1a9564
rk57475 <90454051+rk57475@users.noreply.github.com>
2021-09-14 18:10:36 +0530

    Add files via upload

---
+++ TreniryOperatorMaxNo.java
@@ -0,0 +1,14 @@
+
+public class TreniryOperatorMaxNo {
+
+	public static void main(String[] args) {
+		int x=50;
+		int y=40;
+		int max=0;
+		
+		
+		max=(x>y)?x:y;
+		System.out.println(max);
+	}
+
+}
