c5eb999b4835e9bd862a7618c477029de1b03a07
rk57475 <90454051+rk57475@users.noreply.github.com>
2021-09-15 12:06:10 +0530

    Add files via upload

---
+++ CountDigits.java
@@ -0,0 +1,13 @@
+
+public class CountDigits {
+
+	public static void main(String[] args) {
+		 int a[] = {5,8,2,0,5,0,6,7}, count =0,i;
+	     for(i=0; i <a.length;i++) {
+	    	 count = count+1 ;
+	    	 
+	     }
+	    	 System.out.println("number of digits in array is "+count);
+	}
+
+}
