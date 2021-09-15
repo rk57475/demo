13a630f86a9c03fe2fc8609577441fd9ccaa5216
rk57475 <90454051+rk57475@users.noreply.github.com>
2021-09-15 11:39:30 +0530

    Add files via upload

---
+++ PrimeNumber.java
@@ -0,0 +1,19 @@
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+	
+		
+		int num=11;
+		
+		if(num % 1==0) 
+		{
+			if(num % num == 0)
+			{System.out.println(num + " is a prime no.");}
+			
+			else 
+				System.out.println(num + "is not a prime no");
+		}
+		
+		}
+	}
